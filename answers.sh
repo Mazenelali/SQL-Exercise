2634255b9240cba2862a679c316b4b867bf25378
Mazen El Ali <mazenelali>
2022-12-12 16:00:03 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,21 @@
+#  first step :
+ SELECT name FROM students
+
+# seconde step :
+  select * from database 
+  where age > 30;
+#third step :
+  select female from name 
+   where age >30;
+#step four
+  insert into database (name ,age )
+  value (mazen ,22)
+#step five 
+ select Points from students
+where id =1
+#step six
+ UPDATE students set points =400
+ WHERE id = 2
+#step seven
+  UPDATE students set points = 100
+ WHERE id = 1
